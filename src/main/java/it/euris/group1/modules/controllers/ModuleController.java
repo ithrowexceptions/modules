65d5dbacacb73058560dc0ed633979cbd01cd3ba
andrea.schiavi <andrea.schiavi77@gmail.com>
2019-09-02 17:21:12 +0200

    create a REST controller

---
+++ src/main/java/it/euris/group1/modules/controllers/ModuleController.java
@@ -0,0 +1,10 @@
+package it.euris.group1.modules.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/modules")
+public class ModuleController {
+}
