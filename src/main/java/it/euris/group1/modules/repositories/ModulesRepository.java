d8e19ee98778a3dcc16e9125a1d33813aa4cc23b
AlessandroGondolo <51167069+AlessandroGondolo@users.noreply.github.com>
2019-09-02 17:43:05 +0200

    extend repository from jpa repo generics

--- src/main/java/it/euris/group1/modules/repositories/ModulesRepository.java
+++ src/main/java/it/euris/group1/modules/repositories/ModulesRepository.java
@@ -1,5 +1,7 @@
 package it.euris.group1.modules.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ModulesRepository  extends JpaRepository {}
+@Repository
+public interface ModulesRepository  extends JpaRepository<Module,Long> {}
