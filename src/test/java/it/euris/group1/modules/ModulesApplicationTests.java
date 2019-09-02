e1e6a71213087413bf2af4bc5a82e5d87b1c0314
andrea.schiavi <andrea.schiavi77@gmail.com>
2019-09-02 16:23:32 +0200

    commit project scaffold

---
+++ src/test/java/it/euris/group1/modules/ModulesApplicationTests.java
@@ -0,0 +1,16 @@
+package it.euris.group1.modules;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModulesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
