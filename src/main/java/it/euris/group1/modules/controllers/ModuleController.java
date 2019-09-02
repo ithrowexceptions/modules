9af6c6447627e375113632d3f796e8449eab06d4
andrea.schiavi <andrea.schiavi77@gmail.com>
2019-09-02 17:47:13 +0200

    add get mapping by id

--- src/main/java/it/euris/group1/modules/controllers/ModuleController.java
+++ src/main/java/it/euris/group1/modules/controllers/ModuleController.java
@@ -1,10 +1,24 @@
 package it.euris.group1.modules.controllers;
 
+import it.euris.group1.modules.repositories.ModulesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/modules")
 public class ModuleController {
+    @Autowired
+    private ModulesRepository modulesRepository;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Module> getEmployeeById(@PathVariable("id") Long moduleId) throws Throwable {
+        Module module = modulesRepository
+                .findById(moduleId)
+                .orElseThrow(() -> new ModuleNotFoundException("Module not found for id: " + moduleId));
+        return ResponseEntity.ok().body(module);
+    }
 }
