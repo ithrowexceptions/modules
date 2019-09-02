1a59d9c3a351c6176a0fad1954ce00ee0b9fbdad
andrea.schiavi <andrea.schiavi77@gmail.com>
2019-09-02 18:00:42 +0200

    add controller unit test

---
+++ src/test/java/it/euris/group1/modules/controllers/ModuleControllerTests.java
@@ -0,0 +1,10 @@
+package it.euris.group1.modules.controllers;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModuleControllerTests {
+}
