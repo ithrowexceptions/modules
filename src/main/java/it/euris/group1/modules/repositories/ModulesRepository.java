ac597cc92258c4477a48cfd7015b188c02118604
AlessandroGondolo <51167069+AlessandroGondolo@users.noreply.github.com>
2019-09-03 17:50:55 +0200

    add methods

--- src/main/java/it/euris/group1/modules/repositories/ModulesRepository.java
+++ src/main/java/it/euris/group1/modules/repositories/ModulesRepository.java
@@ -4,5 +4,19 @@ import it.euris.group1.modules.entities.Module;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
-public interface ModulesRepository  extends JpaRepository<Module,Long> {}
+public interface ModulesRepository  extends JpaRepository<Module,Long> {
+
+
+        List<Module> findByName(String name);
+        List<Module> findBySurname(String surname);
+        List<Module> findByBirthDate(LocalDate birthDate);
+        List<Module> findByAge(Integer age);
+        List<Module> findByType (Enum type);
+        //List<Module> findByTimestamp(Timestamp timestamp);
+
+
+}
