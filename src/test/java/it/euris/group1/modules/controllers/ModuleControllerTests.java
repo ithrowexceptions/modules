e8268d4f702f1fdf1b3dabe1e0b8ed08e15b91db
andrea.schiavi <andrea.schiavi77@gmail.com>
2019-09-03 10:30:19 +0200

    implement test on mock mvc context and on fetching entity with id: 1

--- src/test/java/it/euris/group1/modules/controllers/ModuleControllerTests.java
+++ src/test/java/it/euris/group1/modules/controllers/ModuleControllerTests.java
@@ -1,10 +1,83 @@
 package it.euris.group1.modules.controllers;
 
+import it.euris.group1.modules.entities.Module;
+import it.euris.group1.modules.more.Type;
+import it.euris.group1.modules.repositories.ModulesRepository;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//@SpringBootTest
+@WebMvcTest(value = ModuleController.class)
 public class ModuleControllerTests {
+    private List<Module> mockModules;
+    private static final String BASE_URL = "/modules";
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ModulesRepository mockModulesRepository;
+
+    @Before
+    public void setUp() {
+        mockModules = getMockModules();
+        
+        var jason = Optional.of(mockModules.get(0));
+
+//        when(modulesRepository.findById(1L)).thenReturn(jason);
+        doReturn(jason).when(mockModulesRepository).findById(1L);
+    }
+
+    @Test
+    public void contextLoad() {
+        assertNotNull(mvc);
+    }
+
+    @Test
+    public void whenModuledIsProvided_thenRetrievedNameIsCorrect() throws Exception {
+        Long id = 1L;
+
+        MvcResult result = mvc.perform(get(BASE_URL + "/{id}", id).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is("Jason")))
+                .andExpect(jsonPath("$.surname", is("Smith")))
+//                .andExpect(jsonPath("$.birthdate", is()))
+//                .andExpect(jsonPath("$.creationTimestamp", is()))
+                .andExpect(jsonPath("$.age", is(19)))
+                .andExpect(jsonPath("$.type", is(Type.OWNER)))
+                .andReturn();
+    }
+
+    private List<Module> getMockModules() {
+        return List.of(new Module(1L,
+                "Jason",
+                "Smith",
+                LocalDate.of(2000, 01, 01),
+                Timestamp.valueOf("2015-01-01 12:00:00.000"),
+                19,
+                Type.OWNER));
+    }
 }
