bc4b80bb73078b545956f671c9760473b1132e66
andrea.schiavi <andrea.schiavi77@gmail.com>
2019-09-04 14:36:40 +0200

    add more mock data to unit test

--- src/test/java/it/euris/group1/modules/controllers/ModuleControllerUnitTests.java
+++ src/test/java/it/euris/group1/modules/controllers/ModuleControllerUnitTests.java
@@ -41,7 +41,7 @@ public class ModuleControllerUnitTests {
     @Before
     public void setUp() {
         mockModules = getMockModules();
-        
+
         var jason = Optional.of(mockModules.get(0));
 
 //        when(modulesRepository.findById(1L)).thenReturn(jason);
@@ -77,12 +77,17 @@ public class ModuleControllerUnitTests {
     }
 
     private List<Module> getMockModules() {
-        return List.of(new Module(1L,
-                "Jason",
-                "Smith",
-                LocalDate.of(2000, 01, 01),
-                Timestamp.valueOf("2015-01-01 12:00:00.000"),
-                19,
-                Type.OWNER));
+        return List.of(
+                new Module(1L,"Jason","Smith", LocalDate.of(2000, 1, 1), Timestamp.valueOf("2015-01-01 12:00:00.000"), 19, Type.OWNER),
+                new Module(2L,"Alice","Anderson", LocalDate.of(2000, 1, 1), Timestamp.valueOf("2019-02-02 01:01:01.001"), 19, Type.CHILD),
+                new Module(3L,"Bob","Anderson", LocalDate.of(1980, 2, 2), Timestamp.valueOf("2018-03-03 02:02:01:002"), 39, Type.SPOUSE),
+                new Module(4L,"Bob","Carlsen", LocalDate.of(1980, 3, 3), Timestamp.valueOf("2017-04-04 03:03:03:003"), 39, Type.SPOUSE),
+                new Module(5L,"Dean","Dobro", LocalDate.of(2000, 4, 4), Timestamp.valueOf("2018-05-05 04:03:04.004"), 19, Type.CHILD),
+                new Module(6L,"Ester","Effenbach", LocalDate.of(2010, 4, 5), Timestamp.valueOf("2017-06-06 15:05:05.005"), 9, Type.OWNER),
+                new Module(7L,"Filip","Fjord", LocalDate.of(1950, 6, 6), Timestamp.valueOf("2016-07-07 15:06:06.006"), 69, Type.SPOUSE),
+                new Module(8L,"Greta","Gunderson", LocalDate.of(1960, 7, 6), Timestamp.valueOf("2014-08-08 17:07:07.007"), 59, Type.CHILD),
+                new Module(9L,"Hans","Haskell", LocalDate.of(1975, 8, 8), Timestamp.valueOf("2013-09-09 12:00:00.000"), 44, Type.OWNER),
+                new Module(10L,"Bob","Anderson", LocalDate.of(1985, 9, 9), Timestamp.valueOf(" 2016-08-08 00:00:00.000"), 34, Type.SPOUSE)
+        );
     }
 }
