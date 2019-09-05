9be80bff3182e5e99d2d8df90cbdd34f0f781b40
AlessandroGondolo <51167069+AlessandroGondolo@users.noreply.github.com>
2019-09-05 12:02:37 +0200

    add

--- src/main/java/it/euris/group1/modules/repositories/ModulesRepository.java
+++ src/main/java/it/euris/group1/modules/repositories/ModulesRepository.java
@@ -4,6 +4,7 @@ import it.euris.group1.modules.entities.Module;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -16,7 +17,7 @@ public interface ModulesRepository  extends JpaRepository<Module,Long> {
         List<Module> findByBirthDate(LocalDate birthDate);
         List<Module> findByAge(Integer age);
         List<Module> findByType (Enum type);
-        //List<Module> findByTimestamp(Timestamp timestamp);
+       // List<Module> findByTimestamp(Timestamp timestamp);
 
 
 }
