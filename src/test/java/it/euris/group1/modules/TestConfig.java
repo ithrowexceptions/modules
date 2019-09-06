c65f23e50f10616d98ed5bdce21df3fbbc87eccc
AlessandroGondolo <51167069+AlessandroGondolo@users.noreply.github.com>
2019-09-06 11:47:47 +0200

    fix tests

--- src/test/java/it/euris/group1/modules/TestConfig.java
+++ src/test/java/it/euris/group1/modules/TestConfig.java
@@ -18,19 +18,23 @@ public class TestConfig {
     @Bean
     public CommandLineRunner demo(ModulesRepository repository) {
 
-        List<Timestamp> timestamps = new ArrayList<Timestamp>();
-        timestamps.add(Timestamp.valueOf("2015-01-01 12:00:00.000"));
-        timestamps.add(Timestamp.valueOf("2019-02-02 01:01:01.001"));
-        timestamps.add(Timestamp.valueOf("2018-03-03 02:02:01:002"));
-        timestamps.add(Timestamp.valueOf("2017-04-04 03:03:03:003"));
-        timestamps.add(Timestamp.valueOf("2018-05-05 04:03:04.004"));
-        timestamps.add(Timestamp.valueOf("2017-06-06 15:05:05.005"));
-        timestamps.add(Timestamp.valueOf("2016-07-07 15:06:06.006"));
-        timestamps.add(Timestamp.valueOf("2014-08-08 17:07:07.007"));
-        timestamps.add(Timestamp.valueOf("2013-09-09 12:00:00.000"));
-        timestamps.add(Timestamp.valueOf("2016-08-08 00:00:00.000"));
+        List<Timestamp> timestamps = new ArrayList<>(10);
 
         return args -> {
+
+            timestamps.add(Timestamp.valueOf("2015-01-01 12:00:00.000"));
+            timestamps.add(Timestamp.valueOf("2019-02-02 01:01:01.001"));
+            timestamps.add(Timestamp.valueOf("2018-03-03 02:02:01.002"));
+            timestamps.add(Timestamp.valueOf("2017-04-04 03:03:03.003"));
+            timestamps.add(Timestamp.valueOf("2018-05-05 04:03:04.004"));
+            timestamps.add(Timestamp.valueOf("2017-06-06 15:05:05.005"));
+            timestamps.add(Timestamp.valueOf("2016-07-07 15:06:06.006"));
+            timestamps.add(Timestamp.valueOf("2014-08-08 17:07:07.007"));
+            timestamps.add(Timestamp.valueOf("2013-09-09 12:00:00.000"));
+            timestamps.add(Timestamp.valueOf("2016-08-08 00:00:00.000"));
+
+
+
             //save some modules
             repository.save(new Module(1L, "Jason", "Smith", LocalDate.of(2000, 1, 1), timestamps.get(0), 19, Type.OWNER));
             repository.save(new Module(2L, "Alice", "Anderson", LocalDate.of(2000, 1, 1), timestamps.get(1), 19, Type.CHILD));
@@ -42,6 +46,7 @@ public class TestConfig {
             repository.save(new Module(8L, "Greta", "Gunderson", LocalDate.of(1960, 7, 6), timestamps.get(7), 59, Type.CHILD));
             repository.save(new Module(9L, "Hans", "Haskell", LocalDate.of(1975, 8, 8), timestamps.get(8), 44, Type.OWNER));
             repository.save(new Module(10L, "Bob", "Anderson", LocalDate.of(1985, 9, 9), timestamps.get(9), 33, Type.SPOUSE));
+            repository.flush();
 
         };
 
